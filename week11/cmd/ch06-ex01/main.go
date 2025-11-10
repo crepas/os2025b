9f71a9fd517b7db918d83a69848471c25bb8b013
kimsw <kimsw021104@gmail.com>
2025-11-10 14:45:56 +0900

    week11 slice v0.2

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -3,10 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var subjects []string
-	subjects = make([]string, 3)
-	subjects[0] = "Go"
-	subjects[2] = "Python"
+	//방법1
+	// var subjects []string
+	// subjects = make([]string, 3)
+	// subjects[0] = "Go"
+	// subjects[2] = "Python"
+
+	//방법2
+	// subjects := make([]string, 3)
+
+	// subjects[0] = "Go"
+	// subjects[2] = "Python"
+
+	//방법3
+	subjects := []string{"Go", "", "Python"} // 슬라이스 리터럴
 
 	for _, subject := range subjects {
 		fmt.Println(subject)
